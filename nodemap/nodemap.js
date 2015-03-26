483ce52f943f78860e08adceab7accec69ebf2c2
Marco A <marco.diiga@gmail.com>
2015-03-26 23:06:16 +0100

    Added subnodes anchors, reduced API clutter. Still no positioning/connections. Next step is probably positioning the elements.

--- nodemap/nodemap.js
+++ nodemap/nodemap.js
@@ -2,20 +2,32 @@
   'use strict';
 
   // ---------- Node class and methods ----------
-  Node = function (mapObject, parentNode, nodeName, href) {
+  Node = function ($map, $parent, nodeName, href) {
 
-    this.name = nodeName;
-    this.href = href;
+    this.$map     = $map;
+    this.name     = nodeName;
+    this.href     = href;
+    this.$parent  = $parent;
+    this.children = []; // No children for us for now
 
     // Create the node element in the page and append it to the map's object
     this.element = $('<a href="' + this.href + '">' + this.name + '</a>')
       .addClass('node');
-    mapObject.prepend (this.element);
+    this.$map.prepend (this.element);
 
     // The node will be moved directly from code
     this.element.css ('position', 'absolute');
 
-    if (parentNode == null) {} // I'm the root TODO something
+    if (this.$parent == null) // Check if I'm the root
+      this.element.addClass('root');
+    else {
+      this.$parent.node.children.push (this); // Add me as a child of my parent
+      console.log("TODO line");//TODO if children add a line from me to parent to mapObject
+    }
+
+    // TODO activate draggable jquery ui
+
+    //TODO click stuff
   }
 
   // Generate a node map on the jQuery selected objects
@@ -30,30 +42,52 @@
 
     var $window = $(window);
 
-    return this.each(function () { // Generate a node map
+    return this.each(function () { // Generate a node map (returns this)
         // Set up a drawing area
         if (options.mapArea.width === initialSize)
           options.mapArea.width = $window.width();
         if (options.mapArea.height === initialSize)
           options.mapArea.height = $window.height();
 
-        // Add a canvas to the selector
-        this.canvas = Raphael (0, 0, options.mapArea.width,
-                                     options.mapArea.height);
+        // Add a canvas to the selector of this element
+        $(this).append( Raphael (0, 0, options.mapArea.width,
+                                       options.mapArea.height) );
 
         // Add a class to the map DOM object to let node styles be applied
         $(this).addClass ('nodemap');
       });
   };
 
-  $.fn.addRootNode = function (nodeName) {
-    var node = new Node (this,      // Map object (might have multiple DOM
-                                    // elements attached)
-                         null,      // No parent (root)
-                         nodeName,  // nodeName
-                         '/');      // h
-    this.root = node;
-    return jQuery;
+  $.fn.createNodes = function () { // Create the nodes of the map recursively
+    var $rootli = $('>ul>li', this);
+    // Create the root node
+    var $rootNode = $('>a', $rootli).addNode (this, null);
+    var $map = this; // Save 'this' (selector map) for the closure
+    // Foreach sub-anchor item of the root li item -> add a node
+    $('>ul>li>a', $rootli).each (function() {
+      $(this).addNode ($map, $rootNode);
+    });
+  };
+
+
+/*
+  $.fn.addRootNode = function () {
+    var node = new Node (null,                // No parent (root)
+                         this.text(),         // nodeName
+                         this.attr('href'));  // href
+    this.node = node;
+    return this;
+  };
+  */
+
+
+  $.fn.addNode = function (map, parent) {
+    var node = new Node (map,                 // map selector (to add <a>s)
+                         parent,              // parent <a> selector
+                         this.text(),         // nodeName
+                         this.attr('href'));  // href
+    this.node = node;
+    return this;
   };
 
 } (jQuery));
