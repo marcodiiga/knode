23b88b9259cb65c5d329db40de4f285529fd3182
Marco A <marco.diiga@gmail.com>
2015-03-28 19:28:55 +0100

    Added movement logic with Coulomb/Hooke laws - multinode still not working, needs fixing

--- nodemap/nodemap.js
+++ nodemap/nodemap.js
@@ -1,22 +1,26 @@
 (function ($) {
   'use strict';
 
-  var MOVEMENT_TIMEOUT = 4;        // 4 seconds for the nodes' movement timeout
-  var CHILD_PARENT_INIT_DISTANCE = 5; // Starting child<->parent distance
+  var MOVEMENT_TIMEOUT = 4; // 4 seconds for the nodes' movement timeout
+  var CHILD_PARENT_INIT_DISTANCE = 250; // Starting child<->parent distance
+  var NODES_REPULSIVE_FACTOR = 3000;
+  var LINES_RESTORING_FACTOR = 30000;
 
-  // ---------- Node class and methods ----------
-  Node = function ($map, $parent, nodeName, href) { // Typical scope: Node object
+  // ---------- Node and Line class and methods ----------
 
-    this.$map       = $map;
-    this.name       = nodeName;
-    this.href       = href;
-    this.$parent    = $parent; // null for root object, $('a') for the others
-    this.children   = [];      // Our children will populate this for us
-    this.moveTimer  = 0;       // Timer callback to move this node to equilibrium
-    this.stopMoving = false;   // Whether this node should stop moving
+  var Node = function ($map, $parent, nodeName, href) { // Typical scope: Node object
+
+    this.$map         = $map;
+    this.name         = nodeName;
+    this.href         = href;
+    this.$parent      = $parent; // null for root object, $('a') for the others
+    this.children     = [];      // Our children will populate this for us
+    this.hasPosition  = false;   // Has this node been assigned an initial position?
+    this.moveTimer    = 0;       // Timer callback to move this node to equilibrium
+    this.dx = this.dy = 0;     // Force vector (tells this node where to go)
 
     // Create the node element in the page and append it to the map's object
-    this.$element = $('<a href="' + this.href + '">' + this.name + '</a>')
+    this.$element = $('<a href="' + this.href + '"><p>' + this.name + '</p></a>')
       .addClass('node');
     this.$map.prepend (this.$element);
 
@@ -25,11 +29,12 @@
 
     // Note: Adjusted positions are comprehensive of an element's width and height
     if ($parent != null) { // Position in the map area. By default the parent's
-      this.x = $parent.node.x; // position is given..
-      this.y = $parent.node.y;
-    } else { // ..but if the node is the root it is put in the center
+      this.x = $parent.node.x; // position is given (this will have to be)
+      this.y = $parent.node.y; // recalculated..
+    } else { // ..but if the node is the root it is put in the center..
       this.x = ($map.options.mapArea.width / 2);
       this.y = ($map.options.mapArea.height / 2);
+      this.hasPosition = true; // ..and no additional calculations are needed
     }
 
     // The node will be moved directly from code into the map
@@ -39,121 +44,283 @@
       this.$element.addClass('root');
       this.$map.rootNode = this;
       // Immediately set the root element position to the center of the area
-      this.position ();
+      this.takePosition ();
     } else {
       this.$parent.node.children.push (this); // Add me as a child of my parent
-
-      //TODO if children add a line from me to parent to mapObject
+      // Add a line from this node to my parent
+      $map.lines[$map.lines.length] = new Line ($map, this, this.$parent.node);
     }
 
-    // TODO activate draggable jquery ui
+    var thisNode = this;
+    this.$element.draggable ({ // Set a jQuery callback function when dragging
+      drag: function () {
+        thisNode.animateToStaticPosition ();
+      }
+    });
 
     //TODO click stuff
   }
 
-  // Position the node in the (x;y) position keeping into account the element's
-  // width and height
-  Node.prototype.position = function () {
+
+  // Returns 'true' if I reached stability (i.e. an equilibrium point where
+  // all the forces are below a threshold), otherwise return 'false'
+  Node.prototype.stabilityReached = function () {
+
+    // To determine if I reached stability I need to calculate my resulting
+    // force vector and compare it against some minimum thresholds
+    var forceVector = this.calculateForceVector ();
+    // Save the resulting vector components (these will move our position)
+    this.dx = forceVector.dx * 10;
+    this.dy = forceVector.dy * 10;
+
+    // Set to zero insignificant forces
+    if (Math.abs(this.dx) < this.$map.options.minimumForceThreshold)
+      this.dx = 0;
+    if (Math.abs(this.dy) < this.$map.options.minimumForceThreshold)
+      this.dy = 0;
+    if (Math.abs(this.dx) + Math.abs(this.dy) === 0) { // If both are insignificant
+      this.dx = this.dy = 0;
+      console.log("Node " + this.name + " has reached equilibrium");
+      return true; // Yes, I've reached equilibrium
+    }
+
+    return false; // State that I haven't reached stability yet
+  }
+
+  // Position a node in the (x;y) coordinates by taking into account the element's
+  // dimensions and the force components that are asking to move it (dx;dy)
+  Node.prototype.takePosition = function () {
+
+    if (this.dx !== 0) // Apply dx and dy components if present
+      this.x += this.dx * 10;
+    if (this.dy !== 0)
+      this.y += this.dy * 10;
+
     this.adjustedX = this.x - (this.elementWidth / 2);
     this.adjustedY = this.y - (this.elementHeight / 2);
-    this.$element.css({'left': this.adjustedX + "px",
+    this.$element.css({'left': this.adjustedX + "px",  // Finally position me
                        'top':  this.adjustedY + "px" });
   }
 
-  // Calculate this node's equilibrium position relative to its father's
-  Node.prototype.calculateEquilibriumPoint = function (index) {
+  // Lay off radially this node around its father's position
+  Node.prototype.layNodeAroundParent = function (index) {
     var stepAngle = Math.PI * 2 / this.$parent.node.children.length; // Radiants
     var angle = index * stepAngle;
     this.x = (CHILD_PARENT_INIT_DISTANCE * Math.cos(angle)) + this.$parent.node.x;
     this.y = (CHILD_PARENT_INIT_DISTANCE * Math.sin(angle)) + this.$parent.node.y;
-  };
+  }
 
-  // Starts from a node and recursively sets its and its children's
-  // positions over time to a static position
+  // Sets the wheels in motion for all the children of a specific node in order
+  // for them to reach a final static equilibrium position
+  //
+  // This function has two goals:
+  //  1) Calculate the initial equilibrium position for each node of the graph
+  //     (children are as close to their parents as specified by the variable
+  //     CHILD_PARENT_INIT_DISTANCE)
+  //  2) Start an animation loop which computes force vectors per each node and
+  //     apply them to simulate movement
   Node.prototype.animateToStaticPosition = function() {
     // Position of this local root node will be recalculated, stop moving
-    /*
-clearTimeout ($node.node.moveTimer);
 
-    $node.node.moveTimer = setTimeout (function () {
-      $node.node.stopMoving = true; // Stop moving for this node
-    }, MOVEMENT_TIMEOUT * 1000);*/
+
+    // clearTimeout (this.animationLoop);
 
     // Recalculate my children's equilibrium points. Each child will have
     // the final coordinates in (x;y) and (adjustedX, adjustedY)
-    $.each (this.children, function (index) {
-      this.calculateEquilibriumPoint (index);
-      //DEBUG TODO 1) disable this
+    //$.each (this.children, function (index) {
+    //  this.calculateEquilibriumPoint (index);
+    //  this.position ();
+    //});
 
-
-      this.position ();
-
-
-
-    });
-    // TODO 2) set a timer with a callback to transition the current position
-    // of the child nodes to the x;y destination one.
 
     this.animationLoop (); // Start the animation loop
 
-    // TODO 3) Set a threshold to be reached and stop the callback when done
+  }
+
+  // Returns 'true' if the entire subtree (i.e. me and my child nodes)
+  // have reached equilibrium positions
+  Node.prototype.subtreeSeekEquilibrium = function () {
+    var stabilityReached = true; // Assume my subtree has reached stability
+
+    // If my children haven't been positioned yet, lay them around me
+    $.each(this.children, function (index) {
+        if (this.hasPosition == false) {
+          this.layNodeAroundParent (index);
+          this.takePosition ();
+          this.hasPosition = true;
+        }
+    });
+
+    // There's no point in asking if I'm stable if I'm being dragged.
+    // If this element is being dragged by jQuery UI, just store its position
+    if (this.$element.hasClass ("ui-draggable-dragging")) {
+      this.x = parseInt (this.$element.css('left'), 10) +
+        (this.$element.width() / 2); // Adjust the bottom-right dragging pos
+      this.y = parseInt (this.$element.css('top'), 10) +
+        (this.$element.height() / 2);
+      stabilityReached = false; // I'm surely not stable yet
+    } else { // If I'm not being dragged, I might be already stable
+      stabilityReached = this.stabilityReached() && stabilityReached; // Am I stable?
+      if (stabilityReached == false) // If I haven't reached stability, at least
+        this.takePosition (); // get me closer to achieve it
+    }
+
+    // Do the same for every other children: seek stability recursively
+    for (var i = 0; i < this.children.length; ++i) {
+      stabilityReached = this.children[i].subtreeSeekEquilibrium() && stabilityReached;
+    }
+    return stabilityReached;
   }
 
   // This function will call itself repeatedly until all children nodes have
   // reached an equilibrium position
   Node.prototype.animationLoop = function () {
-    var thisNode = this;
 
-    // Move children nodes according to their calculated forces
-    for (var i = 0; i < this.children.length; ++i) {
-      var forceVector = this.children[i].calculateForceVector ();
-      this.children[i].x += forceVector.dx * 2;
-      this.children[i].y += forceVector.dy * 2;
-      this.children[i].position ();
+
+
+    if (this.subtreeSeekEquilibrium ()) {
+      return; // Avoid the callback and just return if our subtree got equilibrium
     }
 
+
+
+
+
+
+
+    var thisNode = this;
     setTimeout(function () {
       thisNode.animationLoop();
     }, 10);
   }
 
+  // Using Coulomb's Law, calculate a repulsion force from another node
+  Node.prototype.calculateSingleRepulsionForceFromNode = function (otherNode) {
+    var x, y, f, distance, theta, xsign, dx, dy;
+    // Calculate x and y distance components between the nodes
+    x = (otherNode.x - this.x);
+    y = (otherNode.y - this.y);
+    distance = Math.sqrt((x * x) + (y * y)); // Distance between nodes
+    if (Math.abs(distance) < 400) { // Excessive distance means no force
+      // Get the angle between x and y as if they were cos and sin
+      if (x === 0) { // Prevent division by zero
+        theta = Math.PI / 2;
+        xsign = 1;
+      } else {
+        theta = Math.atan (y / x);
+        xsign = x / Math.abs (x); // This sign is needed to correctly
+                                  // orient the repulsive force components.
+                                  // Only the X one is needed due to the atan
+                                  // function graph
+      }
+      // Calculate force for the given distance and apply its components
+      f = NODES_REPULSIVE_FACTOR / (distance * distance);
+      dx = -f * Math.cos (theta) * xsign;
+      dy = -f * Math.sin (theta) * xsign;
+    }
+    return { dx: dx,
+             dy: dy };
+  }
+
+  // Using Hooke's Law, calculate an attractive force towards another node
+  Node.prototype.calculateSingleAttractiveForceTowardsNode = function (otherNode) {
+    var x, y, f, distance, theta, xsign, centerFactor = 1, dx = 0, dy = 0;
+    // Calculate x and y distance components between the nodes
+    x = (otherNode.x - this.x);
+    y = (otherNode.y - this.y);
+    distance = Math.sqrt((x * x) + (y * y)); // Distance between nodes
+    if (Math.abs(distance) > 0) { // If we're on the same spot, no force
+      // Get the angle between x and y as if they were cos and sin
+      if (x === 0) { // Prevent division by zero, see comments above
+        theta = Math.PI / 2;
+        xsign = 1;
+      } else {
+        theta = Math.atan (y / x); // See comments above
+        xsign = x / Math.abs (x);
+      }
+      // Calculate Hooke's force and apply its components
+      f = (15 * distance * 0.03) / LINES_RESTORING_FACTOR;
+      dx = f * Math.cos (theta) * xsign;
+      dy = f * Math.sin (theta) * xsign;
+    }
+    return { dx: dx,
+             dy: dy };
+  }
+
   // Returns an object with dx and dy values representing a force vector for a
   // given node. The force vector is the result of the repulsive forces against
-  // all the other nodes in the graph and attractive forces towards TODO
+  // all the other nodes in the graph and attractive forces towards the nodes
+  // connected to this with a line. There's also an additional force: if this
+  // node is the selected one (isTheSelectedNode is true), it will have a force
+  // that pushes it towards the center of the map
   Node.prototype.calculateForceVector = function () {
-    var x, y, f, distance, theta, xsign, fx = 0, fy = 0;
+    var i, otherEnd, f, fx = 0, fy = 0;
 
-    // Using Coulomb's Law, calculate the repulsive force from every other node
-    for (var i = 0; i < this.$map.nodes.length; ++i) {
-      if (this.$map.nodes[i] === this) // Repulsive force from myself = nonsense
-        continue;
-      /*
-if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
-        continue;*/
+    // Calculate the repulsive force from every other node on my same level
+    // (i.e. the children of my parent node). If I have a parent, obviously..
+    if (this.$parent !== null) {
+      var mySiblingNodes = this.$parent.node.children;
+      for (i = 0; i < mySiblingNodes.length; ++i) {
+        if (mySiblingNodes[i] === this)
+          continue; // Repulsive force from myself = nonsense
+        //
+  //if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
+      //    continue;
 
-      // Calculate x and y distances between the nodes
-      x = (this.$map.nodes[i].x - this.x);
-      y = (this.$map.nodes[i].y - this.y);
-      distance = Math.sqrt((x * x) + (y * y)); // Distance between nodes
-      if (Math.abs(distance) < 400) { // Excessive distance means no force
-        // Get the angle between x and y as if they were cos and sin
-        if (x === 0) { // Prevent division by zero
-          theta = Math.PI / 2;
-          xsign = 1;
-        } else {
-          theta = Math.atan (y / x);
-          xsign = x / Math.abs (x); // This sign is needed to correctly
-                                    // orient the repulsive force components.
-                                    // Only the X one is needed due to the atan
-                                    // function graph
-        }
-        // Calculate force for the given distance and apply its components
-        f = 400 / (distance * distance);
-        fx += -f * Math.cos (theta) * xsign;
-        fy += -f * Math.sin (theta) * xsign;
+        f = this.calculateSingleRepulsionForceFromNode (mySiblingNodes[i]);
+        fx += f.dx;
+        fy += f.dy;
       }
+      // And also the repulsive force from my parent node
+      f = this.calculateSingleRepulsionForceFromNode (this.$parent.node);
+      fx += f.dx;
+      fy += f.dy;
     }
+
+
+
+    // Using Hooke's Law, add an attractive force per each line I'm a part of
+    // (only if they're not my children)
+    for (i = 0; i < this.$map.lines.length; ++i) {
+      otherEnd = null; // The other end of the line I'm attached to (if any)
+      if (this.$map.lines[i].startNode === this)
+        otherEnd = this.$map.lines[i].endNode;
+      else if (this.$map.lines[i].endNode === this)
+        otherEnd = this.$map.lines[i].startNode;
+      else
+        continue; // I'm not a part of this line, keep on searching
+
+      if (this.children.indexOf(otherEnd) !== -1)
+        continue; // This is one of my children, keep on searching
+
+      // TODO: ignore hidden nodes' pulling (visible)
+
+      f = this.calculateSingleAttractiveForceTowardsNode (otherEnd);
+      fx += f.dx;
+      fy += f.dy;
+    }
+
+
+    // If I'm the selected node, push me towards the center of the map (Hooke)
+    if (this.$map.selectedNode === this) {
+      f = this.calculateSingleAttractiveForceTowardsNode ({
+        x: (this.$map.options.mapArea.width / 2),
+        y: (this.$map.options.mapArea.height / 2),
+        centerOfTheMap: true
+      });
+      fx += f.dx;
+      fy += f.dy;
+    }
+
     return {dx: fx, dy: fy};
+  }
+
+
+  // A Line object is a connection between two nodes of the map
+  var Line = function ($map, startNode, endNode) {
+    this.$map = $map;
+    this.startNode = startNode;
+    this.endNode = endNode;
   }
 
   // ---------- Map class and methods ----------
@@ -169,6 +336,7 @@ if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
 
     var initialSize = -1;
     options = $.extend({ // Add default options for our map to use
+      minimumForceThreshold : 0.002,
       mapArea: {
         width:  initialSize,
         height: initialSize
@@ -183,6 +351,8 @@ if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
 
     this.options = options; // Save it for the $map
     this.nodes = []; // A list of all the nodes of the map (necessary later)
+    this.lines = []; // A list of all the connections in the map
+    this.selectedNode = null; // The centered, selected node
 
     // Add a canvas to the selector of this element and save it as a property
     var canvas = Raphael (0, 0, options.mapArea.width,
@@ -194,7 +364,7 @@ if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
     this.addClass ('nodemap');
 
     return this;
-  };
+  }
 
   // Create the nodes of the map recursively
   $.fn.createNodes = function () { // Typical scope: $('body') object
@@ -206,7 +376,7 @@ if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
     $('>ul>li>a', $rootli).each (function() {
       $(this).addNode ($map, $rootNode);
     });
-  };
+  }
 
 
 /*
@@ -224,14 +394,17 @@ if (!this.$map.nodes[i].visible) // TODO: a visible attribute could be useful
     this.node = new Node ($map,      // map to add <a>s, typically $('body')
                           $parent,         // parent <a> selector
                           this.text(),        // nodeName
-                          this.attr('href'));   // href
+                          this.attr('href'));     // href
     $map.nodes[$map.nodes.length] = this.node; // Keep a list of all the nodes
                                                // for some distance calculations
     // Everytime a node is added its parent's subtree needs to be reshaped
     if ($parent != null)
       $parent.node.animateToStaticPosition (); // This node will drag its children
+    else
+      $map.selectedNode = this.node; // TODO: implement a selection mechanism, for now
+                                     // the root is the selected element
 
     return this;
-  };
+  }
 
 } (jQuery));
